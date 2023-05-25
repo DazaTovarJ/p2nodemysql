efbfb496aa685ef3e6611c1fc4efaea5a6f0c5cd
Jaime Daza <jaimedavid250701@outlook.com>
2023-05-24 23:01:44 -0500

    fix(server): Install cors middleware

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ import {pool} from './db.js'
 import usuariosRoutes from './routes/usuarios.routes.js'
 import indexRoutes from './routes/index.routes.js'
 //import './config.js'
-import { PORT } from './config.js'
+import cors from 'cors'
 
 const app=express()
 //const port=3002
 app.use(express.json())
+app.use(cors())
 app.use('/api',usuariosRoutes)
 app.use(indexRoutes)
 app.use((req,res)=>{
